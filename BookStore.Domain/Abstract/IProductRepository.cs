b79a90ff55963e542df8aa16c6c9e1ab969cfe5e
KamilSobien <kamil.sobien@gmail.com>
2018-04-30 22:20:16 +0200

    Administration

--- BookStore.Domain/Abstract/IProductRepository.cs
+++ BookStore.Domain/Abstract/IProductRepository.cs
@@ -4,5 +4,6 @@ using System.Collections.Generic;
 namespace BookStore.Domain.Abstract {
     public interface IProductRepository {
         IEnumerable<Product> Products { get; }
+        void SaveProduct(Product product);
     }
 }
--- BookStore.Domain/Entities/Product.cs
+++ BookStore.Domain/Entities/Product.cs
@@ -1,9 +1,17 @@
-﻿namespace BookStore.Domain.Entities {
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace BookStore.Domain.Entities {
     public class Product {
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+        [Display(Name = "Nazwa")]
         public string Name { get; set; }
+        [DataType(DataType.MultilineText), Display(Name = "Opis")]
         public string Description { get; set; }
+        [Display(Name = "Cena")]
         public decimal Price { get; set; }
+        [Display(Name = "Kategoria")]
         public string Category { get; set; }
     }
 }
